f7bc2392ebef666ab2942145916f5c2fd87b1ea9
Pedro Carraro <134337484+paccarrar0@users.noreply.github.com>
2025-02-25 00:53:08 -0300

    Update application.js

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -11,18 +11,3 @@ document.addEventListener("DOMContentLoaded", function () {
     form.classList.add("was-validated");
   });
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".upload-btn").forEach((button) => {
-    button.addEventListener("click", function () {
-      let id = this.getAttribute("data-id"); // Pega o ID correto
-      let fileInput = document.getElementById("imageUploadInput" + id);
-
-      if (fileInput) {
-        fileInput.click(); // Abre o seletor de arquivos apenas do equipamento clicado
-      } else {
-        console.error("Elemento não encontrado: imageUploadInput" + id);
-      }
-    });
-  });
-});
